eba7d3b10d049849253760dd2245768379a6d1d4
mctechnology17 <mctechnology170318@gmail.com>
2021-03-22 01:28:47 +0100

    Actualizacion

---
+++ update.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+dpkg-scanpackages -m ./debs > Packages;
+#sed -i -e '/^SHA/d' ./Packages;
+bzip2 -c9k ./Packages > ./Packages.bz2;
+printf "Origin: MC Technology's Repo\nLabel: MC Technology\nSuite: stable\nVersion: 1.0\nCodename: MC Technology\nArchitecture: iphoneos-arm\nComponents: main\nDescription: MC Technology's Tweaks\nMD5Sum:\n "$(cat ./Packages | md5sum | cut -d ' ' -f 1)" "$(stat ./Packages --printf="%s")" Packages\n "$(cat ./Packages.bz2 | md5sum | cut -d ' ' -f 1)" "$(stat ./Packages.bz2 --printf="%s")" Packages.bz2\n" >Release;
+ls ./debs/ -t | grep '.deb' | perl -e 'use JSON; @in=grep(s/\n$//, <>); $count=0; foreach $fileNow (@in) { $fileNow = "./debs/$fileNow"; $size = -s $fileNow; $debInfo = `dpkg -f $fileNow`; $section = `echo "$debInfo" | grep "Section: " | cut -c 10- | tr -d "\n\r"`; $name= `echo "$debInfo" | grep "Name: " | cut -c 7- | tr -d "\n\r"`; $version= `echo "$debInfo" | grep "Version: " | cut -c 10- | tr -d "\n\r"`; $package= `echo "$debInfo" | grep "Package: " | cut -c 10- | tr -d "\n\r"`; $time= `date -r $fileNow +%s | tr -d "\n\r"`; @in[$count] = {section=>$section, package=>$package, version=>$version, size=>$size+0, time=>$time+0, name=>$name}; $count++; } print encode_json(\@in)."\n";' > all.packages;
+exit 0;
